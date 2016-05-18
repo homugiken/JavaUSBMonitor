89b80d67bc7ce004eef8d42cfb344081480b2e57
homugiken <homugiken@gmail.com>
2016-05-18 16:38:20 +0800

    Add files via upload

---
+++ src/app/test.java
@@ -0,0 +1,30 @@
+package app;
+
+import app.USBTools;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Starting...");
+		USBTools usb = new USBTools();
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Over!");
+		
+		
+	}
+
+}
